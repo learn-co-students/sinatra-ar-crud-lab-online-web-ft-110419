158f22d25f847ca87fcaaa6144ca56d776ef9fca
Nethelbert <gilblasse@gmail.com>
2019-12-20 06:01:21 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,15 +9,22 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    
+    redirect "/articles"
   end
   
   get '/articles/new' do 
     erb :new 
   end
   
+  
+  get "/articles" do
+    @articles = Article.all
+    erb :index
+  end
+  
   post '/articles' do 
-    redirect "/articles/#{Article.create(params).id}"
+    article = Article.find_or_create_by(params)
+    redirect "/articles/#{article.id}"
   end
   
   get '/articles/:id' do 
@@ -25,6 +32,20 @@ class ApplicationController < Sinatra::Base
     erb :show
   end 
   
+  get "/articles/:id/edit" do
+    @article = Article.find_by(id: params[:id])
+    erb :edit
+  end 
   
+  patch '/articles/:id' do
+    article = Article.find_by(id: params[:id])
+    article.update(params.to_a[1..-1].to_h)
+    redirect "/articles/#{ article.id }"
+  end
+  
+  delete "/articles/:id" do
+    Article.destroy(params[:id])
+    redirect "/articles"
+  end
   
 end
