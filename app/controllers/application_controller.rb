399b4889c40412a4caf5f826bf3f29fa26b536d2
ncaudill27 <nelson.a.caudill@gmail.com>
2020-01-07 21:48:55 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,22 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    
+  end
+
+  get '/articles/new'
+    erb :new
+  end
+
+  post '/create_article' do
+    Article.create(params)
+
+    redirect to('/articles/:id')
+  end
+
+  get '/articles/:id' do
+    @article = Article.all.last
+
+    erb :show
   end
 end
