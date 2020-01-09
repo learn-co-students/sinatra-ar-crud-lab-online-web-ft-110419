869b9ae370dafcc1ea54e2ce03157c04bd034810
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-01-09 14:29:46 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,42 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to '/articles'
+  end
+
+  get '/articles' do
+     @articles = Article.all
+     erb :'articles/index'
+  end
+
+  get '/articles/new' do
+    erb :'articles/new'
+  end
+
+  post '/articles' do
+    article = Article.create(params[:article])
+    redirect "/articles/#{article.id}"
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find_by(id: params[:id])
+    erb :"articles/edit"
+  end
+
+  get '/articles/:id' do
+    @article = Article.find_by(id: params[:id])
+    erb :'articles/show'
+  end
+
+  patch '/articles/:id' do
+    article = Article.find_by(id: params[:id])
+    article.update(params[:article])
+    redirect "/articles/#{article.id}"
+  end
+
+  delete '/articles/:id' do
+    article = Article.find_by(id: params[:id])
+    article.destroy
+    redirect '/articles'
   end
 end
